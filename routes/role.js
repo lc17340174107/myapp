df2fa75b6ff139418ed22d6ba12dffa77941a963
lc17340174107 <50012337+lc17340174107@users.noreply.github.com>
2020-11-08 22:31:04 +0800

    新增删除角色（未完成）接口和新增角色接口（未完成）

--- routes/role.js
+++ routes/role.js
@@ -1,6 +1,9 @@
 var express = require('express');
+const {updateOneById} = require("../mongodb/collection");
+const {findOne} = require("../mongodb/collection");
+const {insertOne} = require("../mongodb/collection");
 const { systemSend } = require('../common/utils');
-const { find, findOne } = require('../mongodb/collection');
+const { find, findOneById, deleteOneById } = require('../mongodb/collection');
 var router = express.Router();
 
 // 查询权限用户列表接口
@@ -16,4 +19,30 @@ router.get('/vue-admin-template/role/list', (req, res) => {
     }
 })
 
+// 新增角色接口 （目前只新增了角色  还没有新增菜单的权限）
+router.post('/vue-admin-template/role/add', (req, res) => {
+    findOne('roles', {role: req.body.role}, (err, result) => {
+        if (err) throw err;
+        if (result != null) {
+            res.send({code: 101, message: '该角色已存在'})
+        } else {
+            insertOne('roles', {role: req.body.role}, (err, result) => {
+                if (err) throw err;
+            })
+        }
+    })
+})
+
+// 删除角色接口
+router.delete('/vue-admin-template/role/deleteOne', (req, res) => {
+    findOneById('roles', req.body._id, (err, result) => {
+        if(err) throw err;
+        if(result != null) {
+            deleteOneById('roles', req.body._id, (err, result) => {
+                if(err) throw err;
+                systemSend(res, {message: "删除成功"})
+            })
+        }
+    })
+})
 module.exports = router;
